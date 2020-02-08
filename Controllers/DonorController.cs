318b3b7c186944cb3a8e590f6e25f662b292c281
Carlos Fournier <cmfournier@gmail.com>
2020-02-08 18:09:04 -0500

    Added functionality for adding new donors to database

--- Controllers/DonorController.cs
+++ Controllers/DonorController.cs
@@ -3,15 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using SimpleCRM.Models;
+using SimpleCRM.ViewModels;
+using SimpleCRM.Data;
 
 namespace SimpleCRM.Controllers
 {
     public class DonorController : Controller
     {
+        private readonly ApplicationDbContext context;
+        public DonorController(ApplicationDbContext dbcontext)
+        {
+            context = dbcontext;
+        }
         public IActionResult Index()
         {
             return View();
         }
+
+        public IActionResult Add()
+        {
+            AddDonorViewModel addDonorViewModel = new AddDonorViewModel();
+            return View(addDonorViewModel);
+        }
+
+
+        [HttpPost]
+        public IActionResult Add(AddDonorViewModel addDonorViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                Donor newDonor = new Donor()
+                {
+                    Name = addDonorViewModel.Name,
+                    Address = addDonorViewModel.Address,
+                    City = addDonorViewModel.City,
+                    State = addDonorViewModel.State,
+                    Zip = addDonorViewModel.Zip
+                };
+                context.Donors.Add(newDonor);
+                context.SaveChanges();
+                return Redirect("/Donor");
+            }
+            return View();
+        }
     }
 }
+
+
+       
+
+
+
--- ViewModels/AddDonorViewModel.cs
+++ ViewModels/AddDonorViewModel.cs
@@ -1,11 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using SimpleCRM.Models;
 
 namespace SimpleCRM.ViewModels
 {
     public class AddDonorViewModel
     {
+        public int DDonor { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
     }
 }
