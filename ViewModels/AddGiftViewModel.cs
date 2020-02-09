1382f195894889cb5d6bbf944599abf048be6462
Carlos Fournier <cmfournier@gmail.com>
2020-02-09 18:32:25 -0500

    Added form to Gift Entry landing page

---
+++ ViewModels/AddGiftViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleCRM.ViewModels
+{
+    public class AddGiftViewModel
+    {
+        public int TDonor { get; set; }
+        public string TYear { get; set; }
+        public string TMonth { get; set; }
+        public string TDay { get; set; }
+        public float TAmount { get; set; }
+        public string TCampaign { get; set; }
+    }
+}
