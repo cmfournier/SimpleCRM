a551f9fc8bfbad4a8489c219b2378059ccaf75df
Carlos Fournier <cmfournier@gmail.com>
2020-02-10 19:30:00 -0500

    Added form validation for adding donors

--- ViewModels/AddDonorViewModel.cs
+++ ViewModels/AddDonorViewModel.cs
@@ -10,10 +10,16 @@ namespace SimpleCRM.ViewModels
     public class AddDonorViewModel
     {
         public int DDonor { get; set; }
+        [Required(ErrorMessage = "Donor name is required")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Address is required")]
         public string Address { get; set; }
+        [Required(ErrorMessage = "City is required")]
         public string City { get; set; }
+        [Required(ErrorMessage = "State is required")]
         public string State { get; set; }
+        [Required(ErrorMessage = "You must enter a valid ZIP Code")]
+        [Display(Name="ZIP Code")]
         public string Zip { get; set; }
     }
 }
